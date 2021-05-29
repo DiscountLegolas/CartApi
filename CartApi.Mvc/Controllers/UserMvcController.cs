a17a6a8340ab112404e83015953c5ba6daddbab1
DiscountLegolas <69970115+DiscountLegolas@users.noreply.github.com>
2021-05-29 22:10:13 +0300

    Something

---
+++ CartApi.Mvc/Controllers/UserMvcController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CartApi.Mvc.Controllers
+{
+    public class UserMvcController : Controller
+    {
+        // GET: UserMvc
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login()
+    }
+}
